2db90c0fb1a5e6aba6fbad1f3d8e828332b87bba
Erin Mettille <ecmettille@gmail.com>
2020-04-17 20:53:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,22 @@
 #A prime number is a natural number greater than 1 that cannot be formed by multiplying two smaller natural numbers. 
+    #if the resulting number is whole - not prime
+    #if the resulting number is not whole - prime
+    
 
 def prime?(x)
   all_numbers = [2..(x-1)]
   
   when x < 1
     FALSE
-  when x > 1
-    all_numbers.all? do |n| 
-      result = x / n
-      result == 
-    end
-    #if the resulting number is whole - not prime
-    #if the resulting number is not whole - prime
   else
     TRUE
   end
   
 end
+
+  when x > 1
+    all_numbers.all? do |n| 
+      result = x / n
+      if result == all_numbers
+        FALSE
+    end
